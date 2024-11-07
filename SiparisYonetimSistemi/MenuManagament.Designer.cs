43b776f5a4f35990c0becf1c43d087730dd48076
Ardalka <ardailktug@gmail.com>
2024-11-07 13:36:15 +0300

    menu managament başına - eklendi

--- SiparisYonetimSistemi/MenuManagament.Designer.cs
+++ SiparisYonetimSistemi/MenuManagament.Designer.cs
@@ -141,9 +141,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(233, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(197, 25);
+            this.label1.Size = new System.Drawing.Size(204, 25);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Menu Managament";
+            this.label1.Text = "-Menu Managament";
             // 
             // button1
             // 
