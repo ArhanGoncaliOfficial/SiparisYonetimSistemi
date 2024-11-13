87b93080a4362a23ac7e77c1a3c38a47ee6f31a7
Arhan Goncalı <arhangoncaliofficial@gmail.com>
2024-11-13 20:41:37 +0300

    'LoginForm' da Email, Password ile login olunması sağlandı. 'Show Password' özelliği eklendi. Database: SiparisYonetimDB olarak tanımlandı. Form her açıldığında  admin kullanıcı tanımlı değilse sistemde default bir admin kullanıcı oluşturuyor. Default admin kullanıcı Email: admin@example.com Şifre: root

--- SiparisYonetimSistemi/LoginForm.Designer.cs
+++ SiparisYonetimSistemi/LoginForm.Designer.cs
@@ -37,6 +37,7 @@
             this.passwordInputField = new System.Windows.Forms.TextBox();
             this.loginButton = new System.Windows.Forms.Button();
             this.loginFormCloseButton = new System.Windows.Forms.Button();
+            this.showPasswordCheckbox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // banner
@@ -44,9 +45,8 @@
             this.banner.BackColor = System.Drawing.Color.Crimson;
             this.banner.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("banner.BackgroundImage")));
             this.banner.Location = new System.Drawing.Point(0, 0);
-            this.banner.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.banner.Name = "banner";
-            this.banner.Size = new System.Drawing.Size(400, 615);
+            this.banner.Size = new System.Drawing.Size(300, 500);
             this.banner.TabIndex = 0;
             // 
             // formHeader
@@ -54,10 +54,9 @@
             this.formHeader.AutoSize = true;
             this.formHeader.BackColor = System.Drawing.Color.Transparent;
             this.formHeader.Font = new System.Drawing.Font("Edwardian Script ITC", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.formHeader.Location = new System.Drawing.Point(507, 98);
-            this.formHeader.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.formHeader.Location = new System.Drawing.Point(380, 80);
             this.formHeader.Name = "formHeader";
-            this.formHeader.Size = new System.Drawing.Size(424, 94);
+            this.formHeader.Size = new System.Drawing.Size(342, 76);
             this.formHeader.TabIndex = 1;
             this.formHeader.Text = "Welcome Back";
             // 
@@ -66,10 +65,9 @@
             this.emailInputLabel.AutoSize = true;
             this.emailInputLabel.BackColor = System.Drawing.Color.Transparent;
             this.emailInputLabel.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailInputLabel.Location = new System.Drawing.Point(604, 241);
-            this.emailInputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.emailInputLabel.Location = new System.Drawing.Point(453, 196);
             this.emailInputLabel.Name = "emailInputLabel";
-            this.emailInputLabel.Size = new System.Drawing.Size(51, 24);
+            this.emailInputLabel.Size = new System.Drawing.Size(42, 20);
             this.emailInputLabel.TabIndex = 2;
             this.emailInputLabel.Text = "Email";
             // 
@@ -78,10 +76,9 @@
             this.emailInputField.BackColor = System.Drawing.Color.Bisque;
             this.emailInputField.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.emailInputField.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailInputField.Location = new System.Drawing.Point(611, 270);
-            this.emailInputField.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.emailInputField.Location = new System.Drawing.Point(458, 219);
             this.emailInputField.Name = "emailInputField";
-            this.emailInputField.Size = new System.Drawing.Size(267, 27);
+            this.emailInputField.Size = new System.Drawing.Size(200, 22);
             this.emailInputField.TabIndex = 3;
             // 
             // paswordInputLabel
@@ -89,10 +86,9 @@
             this.paswordInputLabel.AutoSize = true;
             this.paswordInputLabel.BackColor = System.Drawing.Color.Transparent;
             this.paswordInputLabel.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.paswordInputLabel.Location = new System.Drawing.Point(607, 313);
-            this.paswordInputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.paswordInputLabel.Location = new System.Drawing.Point(455, 254);
             this.paswordInputLabel.Name = "paswordInputLabel";
-            this.paswordInputLabel.Size = new System.Drawing.Size(82, 24);
+            this.paswordInputLabel.Size = new System.Drawing.Size(68, 20);
             this.paswordInputLabel.TabIndex = 2;
             this.paswordInputLabel.Text = "Password";
             // 
@@ -101,10 +97,9 @@
             this.passwordInputField.BackColor = System.Drawing.Color.Bisque;
             this.passwordInputField.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.passwordInputField.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordInputField.Location = new System.Drawing.Point(612, 341);
-            this.passwordInputField.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.passwordInputField.Location = new System.Drawing.Point(459, 277);
             this.passwordInputField.Name = "passwordInputField";
-            this.passwordInputField.Size = new System.Drawing.Size(267, 27);
+            this.passwordInputField.Size = new System.Drawing.Size(200, 22);
             this.passwordInputField.TabIndex = 3;
             // 
             // loginButton
@@ -114,13 +109,13 @@
             this.loginButton.FlatAppearance.BorderSize = 0;
             this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.loginButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginButton.Location = new System.Drawing.Point(612, 405);
-            this.loginButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loginButton.Location = new System.Drawing.Point(459, 329);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(267, 49);
+            this.loginButton.Size = new System.Drawing.Size(200, 40);
             this.loginButton.TabIndex = 4;
             this.loginButton.Text = "Login";
             this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
             // 
             // loginFormCloseButton
             // 
@@ -129,22 +124,33 @@
             this.loginFormCloseButton.FlatAppearance.BorderSize = 0;
             this.loginFormCloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.loginFormCloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginFormCloseButton.Location = new System.Drawing.Point(1011, 15);
-            this.loginFormCloseButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loginFormCloseButton.Location = new System.Drawing.Point(758, 12);
             this.loginFormCloseButton.Name = "loginFormCloseButton";
-            this.loginFormCloseButton.Size = new System.Drawing.Size(40, 37);
+            this.loginFormCloseButton.Size = new System.Drawing.Size(30, 30);
             this.loginFormCloseButton.TabIndex = 5;
             this.loginFormCloseButton.Text = "X";
             this.loginFormCloseButton.UseVisualStyleBackColor = false;
-            this.loginFormCloseButton.Click += new System.EventHandler(this.button2_Click);
+            this.loginFormCloseButton.Click += new System.EventHandler(this.loginFormCloseButton_Click);
+            // 
+            // showPasswordCheckbox
+            // 
+            this.showPasswordCheckbox.AutoSize = true;
+            this.showPasswordCheckbox.BackColor = System.Drawing.Color.Transparent;
+            this.showPasswordCheckbox.Location = new System.Drawing.Point(557, 305);
+            this.showPasswordCheckbox.Name = "showPasswordCheckbox";
+            this.showPasswordCheckbox.Size = new System.Drawing.Size(102, 17);
+            this.showPasswordCheckbox.TabIndex = 6;
+            this.showPasswordCheckbox.Text = "Show Password";
+            this.showPasswordCheckbox.UseVisualStyleBackColor = false;
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1067, 615);
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.showPasswordCheckbox);
             this.Controls.Add(this.loginFormCloseButton);
             this.Controls.Add(this.loginButton);
             this.Controls.Add(this.passwordInputField);
@@ -154,11 +160,9 @@
             this.Controls.Add(this.formHeader);
             this.Controls.Add(this.banner);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -174,6 +178,7 @@
         private System.Windows.Forms.TextBox passwordInputField;
         private System.Windows.Forms.Button loginButton;
         private System.Windows.Forms.Button loginFormCloseButton;
+        private System.Windows.Forms.CheckBox showPasswordCheckbox;
     }
 }
 
--- SiparisYonetimSistemi/LoginForm.cs
+++ SiparisYonetimSistemi/LoginForm.cs
@@ -1,47 +1,199 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
 
 namespace SiparisYonetimSistemi
 {
-
     public partial class LoginForm : Form
     {
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
         private static extern IntPtr CreateRoundRectRgn
         (
-            int nLeftRect,     // x-coordinate of upper-left corner
-            int nTopRect,      // y-coordinate of upper-left corner
-            int nRightRect,    // x-coordinate of lower-right corner
-            int nBottomRect,   // y-coordinate of lower-right corner
-            int nWidthEllipse, // width of ellipse
-            int nHeightEllipse // height of ellipse
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
         );
+
+        // SQL Connection string (Update as needed)
+        private string connectionString = @"Server=.;Database=SiparisYonetimDB;Trusted_Connection=True;";
 
         public LoginForm()
         {
             InitializeComponent();
-            this.CenterToScreen();
-            //this.FormBorderStyle = FormBorderStyle.Sizable;
-            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 40, 40));
+            InitializeCustomComponents();
+
+            // Form load event handler
+            this.Load += LoginForm_Load;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void LoginForm_Load(object sender, EventArgs e)
         {
-
+            TestSqlConnection();
+            CreateDefaultAdminUser();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void InitializeCustomComponents()
+        {
+            this.CenterToScreen();
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 40, 40));
+
+            passwordInputField.UseSystemPasswordChar = true;
+            showPasswordCheckbox.CheckedChanged += ShowPasswordCheckbox_CheckedChanged;
+        }
+
+        // Toggle password visibility
+        private void ShowPasswordCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            passwordInputField.UseSystemPasswordChar = !showPasswordCheckbox.Checked;
+        }
+
+        // Close the form
+        private void loginFormCloseButton_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        // Test the SQL database connection
+        private void TestSqlConnection()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    MessageBox.Show("Database connection successful.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Connection error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+        }
+
+        // Create default admin user if not exists
+        private void CreateDefaultAdminUser()
+        {
+            string defaultUsername = "root";
+            string defaultPassword = "root";
+            string hashedPassword = ComputeSha256Hash(defaultPassword);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Check if admin user already exists
+                    string checkAdminQuery = "SELECT COUNT(1) FROM Users WHERE username = @username";
+                    using (SqlCommand checkCommand = new SqlCommand(checkAdminQuery, connection))
+                    {
+                        checkCommand.Parameters.Add(new SqlParameter("@username", SqlDbType.NVarChar) { Value = defaultUsername });
+                        int userCount = Convert.ToInt32(checkCommand.ExecuteScalar());
+
+                        // If no admin user exists, create one
+                        if (userCount == 0)
+                        {
+                            string insertAdminQuery = @"INSERT INTO Users (username, email, password_hash, first_name, last_name, phone_number, is_admin, last_login)
+                                                         VALUES (@username, @adminEmail, @hashedPassword, 'Admin', 'User', '000-000-0000', 1, GETDATE())";
+                            using (SqlCommand insertCommand = new SqlCommand(insertAdminQuery, connection))
+                            {
+                                insertCommand.Parameters.Add(new SqlParameter("@username", SqlDbType.NVarChar) { Value = defaultUsername });
+                                insertCommand.Parameters.Add(new SqlParameter("@adminEmail", SqlDbType.NVarChar) { Value = "admin@example.com" });
+                                insertCommand.Parameters.Add(new SqlParameter("@hashedPassword", SqlDbType.NVarChar) { Value = hashedPassword });
+
+                                insertCommand.ExecuteNonQuery();
+                                MessageBox.Show("Default admin user created: \nEmail: admin@example.com\nPassword: root", "Admin User Created", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error creating default admin user: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Login button click event
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string email = emailInputField.Text.Trim(); // Using email only
+            string password = passwordInputField.Text.Trim();
+
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please fill out every input!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Hash the input password
+            string hashedPassword = ComputeSha256Hash(password);
+
+            // Validate the user
+            if (ValidateUser(email, hashedPassword))
+            {
+                MessageBox.Show("Login successful!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Redirect to another form or dashboard
+            }
+            else
+            {
+                MessageBox.Show("Invalid email or password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Validate user credentials using email and hashed password
+        private bool ValidateUser(string email, string hashedPassword)
+        {
+            bool isValidUser = false;
+            string query = @"SELECT COUNT(1) FROM Users 
+                             WHERE email = @Email 
+                             AND password_hash = @hashedPassword";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.Add(new SqlParameter("@Email", SqlDbType.NVarChar) { Value = email });
+                        command.Parameters.Add(new SqlParameter("@hashedPassword", SqlDbType.NVarChar) { Value = hashedPassword });
+
+                        int userCount = Convert.ToInt32(command.ExecuteScalar());
+                        isValidUser = userCount == 1;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Database connection error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            return isValidUser;
+        }
+
+        // Compute SHA256 hash for the given string
+        private string ComputeSha256Hash(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
     }
 }
