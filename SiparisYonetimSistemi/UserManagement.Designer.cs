f23e94e229bc13f27b29c61d4c2b4041076a393c
SubCannon <subcannon_bf44@hotmail.com>
2024-11-17 02:11:36 +0300

    Sidebar, UserControl aracılığıyla optimize edildi.

--- SiparisYonetimSistemi/UserManagement.Designer.cs
+++ SiparisYonetimSistemi/UserManagement.Designer.cs
@@ -28,8 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserManagement));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
@@ -42,25 +41,8 @@
             this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.sidebar = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel8 = new System.Windows.Forms.Panel();
-            this.panel9 = new System.Windows.Forms.Panel();
-            this.overview_btn = new System.Windows.Forms.Button();
-            this.panel10 = new System.Windows.Forms.Panel();
-            this.user_m_btn = new System.Windows.Forms.Button();
-            this.panel11 = new System.Windows.Forms.Panel();
-            this.menu_m_btn = new System.Windows.Forms.Button();
-            this.panel12 = new System.Windows.Forms.Panel();
-            this.acc_btn = new System.Windows.Forms.Button();
-            this.panel13 = new System.Windows.Forms.Panel();
-            this.reports_btn = new System.Windows.Forms.Button();
+            this.sidebar1 = new SiparisYonetimSistemi.sidebar();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.sidebar.SuspendLayout();
-            this.panel9.SuspendLayout();
-            this.panel10.SuspendLayout();
-            this.panel11.SuspendLayout();
-            this.panel12.SuspendLayout();
-            this.panel13.SuspendLayout();
             this.SuspendLayout();
             // 
             // button3
@@ -95,12 +77,12 @@
             // 
             // dataGridView1
             // 
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.White;
-            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ID,
@@ -178,155 +160,21 @@
             this.label1.TabIndex = 12;
             this.label1.Text = "User Management";
             // 
-            // sidebar
+            // sidebar1
             // 
-            this.sidebar.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.sidebar.Controls.Add(this.panel8);
-            this.sidebar.Controls.Add(this.panel9);
-            this.sidebar.Controls.Add(this.panel10);
-            this.sidebar.Controls.Add(this.panel11);
-            this.sidebar.Controls.Add(this.panel12);
-            this.sidebar.Controls.Add(this.panel13);
-            this.sidebar.Dock = System.Windows.Forms.DockStyle.Left;
-            this.sidebar.Location = new System.Drawing.Point(0, 0);
-            this.sidebar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.sidebar.MaximumSize = new System.Drawing.Size(248, 628);
-            this.sidebar.MinimumSize = new System.Drawing.Size(40, 628);
-            this.sidebar.Name = "sidebar";
-            this.sidebar.Size = new System.Drawing.Size(248, 628);
-            this.sidebar.TabIndex = 17;
-            // 
-            // panel8
-            // 
-            this.panel8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel8.BackgroundImage")));
-            this.panel8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel8.Location = new System.Drawing.Point(3, 2);
-            this.panel8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(245, 162);
-            this.panel8.TabIndex = 0;
-            // 
-            // panel9
-            // 
-            this.panel9.Controls.Add(this.overview_btn);
-            this.panel9.Location = new System.Drawing.Point(3, 168);
-            this.panel9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel9.Name = "panel9";
-            this.panel9.Size = new System.Drawing.Size(245, 46);
-            this.panel9.TabIndex = 0;
-            // 
-            // overview_btn
-            // 
-            this.overview_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.overview_btn.ForeColor = System.Drawing.Color.White;
-            this.overview_btn.Location = new System.Drawing.Point(-3, -6);
-            this.overview_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.overview_btn.Name = "overview_btn";
-            this.overview_btn.Size = new System.Drawing.Size(255, 55);
-            this.overview_btn.TabIndex = 4;
-            this.overview_btn.Text = "          Overview";
-            this.overview_btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.overview_btn.UseVisualStyleBackColor = true;
-            this.overview_btn.Click += new System.EventHandler(this.overview_btn_Click);
-            // 
-            // panel10
-            // 
-            this.panel10.Controls.Add(this.user_m_btn);
-            this.panel10.Location = new System.Drawing.Point(3, 218);
-            this.panel10.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel10.Name = "panel10";
-            this.panel10.Size = new System.Drawing.Size(245, 46);
-            this.panel10.TabIndex = 5;
-            // 
-            // user_m_btn
-            // 
-            this.user_m_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.user_m_btn.ForeColor = System.Drawing.Color.White;
-            this.user_m_btn.Location = new System.Drawing.Point(-3, -6);
-            this.user_m_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.user_m_btn.Name = "user_m_btn";
-            this.user_m_btn.Size = new System.Drawing.Size(255, 55);
-            this.user_m_btn.TabIndex = 4;
-            this.user_m_btn.Text = "          User Management      ";
-            this.user_m_btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.user_m_btn.UseVisualStyleBackColor = true;
-            this.user_m_btn.Click += new System.EventHandler(this.user_m_btn_Click);
-            // 
-            // panel11
-            // 
-            this.panel11.Controls.Add(this.menu_m_btn);
-            this.panel11.Location = new System.Drawing.Point(3, 268);
-            this.panel11.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel11.Name = "panel11";
-            this.panel11.Size = new System.Drawing.Size(245, 46);
-            this.panel11.TabIndex = 5;
-            // 
-            // menu_m_btn
-            // 
-            this.menu_m_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.menu_m_btn.ForeColor = System.Drawing.Color.White;
-            this.menu_m_btn.Location = new System.Drawing.Point(-3, -6);
-            this.menu_m_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.menu_m_btn.Name = "menu_m_btn";
-            this.menu_m_btn.Size = new System.Drawing.Size(255, 55);
-            this.menu_m_btn.TabIndex = 4;
-            this.menu_m_btn.Text = "          Menu Management";
-            this.menu_m_btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.menu_m_btn.UseVisualStyleBackColor = true;
-            this.menu_m_btn.Click += new System.EventHandler(this.menu_m_btn_Click);
-            // 
-            // panel12
-            // 
-            this.panel12.Controls.Add(this.acc_btn);
-            this.panel12.Location = new System.Drawing.Point(3, 318);
-            this.panel12.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel12.Name = "panel12";
-            this.panel12.Size = new System.Drawing.Size(245, 46);
-            this.panel12.TabIndex = 5;
-            // 
-            // acc_btn
-            // 
-            this.acc_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.acc_btn.ForeColor = System.Drawing.Color.White;
-            this.acc_btn.Location = new System.Drawing.Point(-3, -6);
-            this.acc_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.acc_btn.Name = "acc_btn";
-            this.acc_btn.Size = new System.Drawing.Size(255, 55);
-            this.acc_btn.TabIndex = 4;
-            this.acc_btn.Text = "          Accountancy";
-            this.acc_btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.acc_btn.UseVisualStyleBackColor = true;
-            this.acc_btn.Click += new System.EventHandler(this.acc_btn_Click);
-            // 
-            // panel13
-            // 
-            this.panel13.Controls.Add(this.reports_btn);
-            this.panel13.Location = new System.Drawing.Point(3, 368);
-            this.panel13.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel13.Name = "panel13";
-            this.panel13.Size = new System.Drawing.Size(245, 46);
-            this.panel13.TabIndex = 6;
-            // 
-            // reports_btn
-            // 
-            this.reports_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.reports_btn.ForeColor = System.Drawing.Color.White;
-            this.reports_btn.Location = new System.Drawing.Point(-3, -6);
-            this.reports_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.reports_btn.Name = "reports_btn";
-            this.reports_btn.Size = new System.Drawing.Size(255, 55);
-            this.reports_btn.TabIndex = 4;
-            this.reports_btn.Text = "          Reports";
-            this.reports_btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.reports_btn.UseVisualStyleBackColor = true;
-            this.reports_btn.Click += new System.EventHandler(this.reports_btn_Click);
+            this.sidebar1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidebar1.Location = new System.Drawing.Point(0, 0);
+            this.sidebar1.Name = "sidebar1";
+            this.sidebar1.ParentFormRef = null;
+            this.sidebar1.Size = new System.Drawing.Size(248, 628);
+            this.sidebar1.TabIndex = 17;
             // 
             // UserManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1182, 628);
-            this.Controls.Add(this.sidebar);
+            this.Controls.Add(this.sidebar1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -336,13 +184,8 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "UserManagement";
             this.Text = "UserManagement";
+            this.Load += new System.EventHandler(this.UserManagement_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.sidebar.ResumeLayout(false);
-            this.panel9.ResumeLayout(false);
-            this.panel10.ResumeLayout(false);
-            this.panel11.ResumeLayout(false);
-            this.panel12.ResumeLayout(false);
-            this.panel13.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -361,17 +204,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Price;
         private System.Windows.Forms.DataGridViewTextBoxColumn Ingredients;
         private System.Windows.Forms.DataGridViewTextBoxColumn Date;
-        private System.Windows.Forms.FlowLayoutPanel sidebar;
-        private System.Windows.Forms.Panel panel8;
-        private System.Windows.Forms.Panel panel9;
-        private System.Windows.Forms.Button overview_btn;
-        private System.Windows.Forms.Panel panel10;
-        private System.Windows.Forms.Button user_m_btn;
-        private System.Windows.Forms.Panel panel11;
-        private System.Windows.Forms.Button menu_m_btn;
-        private System.Windows.Forms.Panel panel12;
-        private System.Windows.Forms.Button acc_btn;
-        private System.Windows.Forms.Panel panel13;
-        private System.Windows.Forms.Button reports_btn;
+        private sidebar sidebar1;
     }
 }
--- SiparisYonetimSistemi/sidebar.cs
+++ SiparisYonetimSistemi/sidebar.cs
@@ -18,46 +18,66 @@ namespace SiparisYonetimSistemi
         {
             InitializeComponent();
         }
+        public Form ParentFormRef { get; set; }
 
-        private void overview_Click(object sender, EventArgs e)
+        private void overview_btn_Click(object sender, EventArgs e)
         {
             HomePageForm homePageForm = new HomePageForm();
             homePageForm.StartPosition = FormStartPosition.CenterScreen;
-            this.Hide();
             homePageForm.Show();
 
+            if (ParentFormRef != null)
+            {
+                ParentFormRef.Hide();
+            }
         }
 
-        private void user_m_Click(object sender, EventArgs e)
+        private void user_m_btn_Click(object sender, EventArgs e)
         {
             UserManagement userManagementForm = new UserManagement();
             userManagementForm.StartPosition = FormStartPosition.CenterScreen;
-            this.Hide();
             userManagementForm.Show();
+
+            if (ParentFormRef != null)
+            {
+                ParentFormRef.Hide();
+            }
         }
 
-        private void menu_m_Click(object sender, EventArgs e)
+        private void menu_m_btn_Click(object sender, EventArgs e)
         {
             MenuManagament menuManagementForm = new MenuManagament();
             menuManagementForm.StartPosition = FormStartPosition.CenterScreen;
-            this.Hide();
             menuManagementForm.Show();
+
+            if (ParentFormRef != null)
+            {
+                ParentFormRef.Hide();
+            }
         }
 
-        private void acc_Click(object sender, EventArgs e)
+        private void acc_btn_Click(object sender, EventArgs e)
         {
             AccountingDetails accountingDetailsForm = new AccountingDetails();
             accountingDetailsForm.StartPosition = FormStartPosition.CenterScreen;
-            this.Hide();
             accountingDetailsForm.Show();
+
+            if (ParentFormRef != null)
+            {
+                ParentFormRef.Hide();
+            }
         }
 
-        private void reports_Click(object sender, EventArgs e)
+        private void reports_btn_Click(object sender, EventArgs e)
         {
             Reports reportsForm = new Reports();
             reportsForm.StartPosition = FormStartPosition.CenterScreen;
-            this.Hide();
             reportsForm.Show();
+
+            if (ParentFormRef != null)
+            {
+                ParentFormRef.Hide();
+            }
         }
     }
 }
