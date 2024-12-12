e2ec5be2739e0250abe8f4d7aada5fe04fbc5de2
SubCannon <subcannon_bf44@hotmail.com>
2024-12-13 01:44:03 +0300

    Sidebar'a SupplierManagement butonu eklendi.

--- SiparisYonetimSistemi/SupplierManagament.cs
+++ SiparisYonetimSistemi/SupplierManagament.cs
@@ -75,6 +75,7 @@ namespace SiparisYonetimSistemi
         private void SupplierManagament_Load(object sender, EventArgs e)
         {
             LoadSuppliers();
+            sidebar1.ParentFormRef = this;
         }
 
         private void AddButton_Click(object sender, EventArgs e)
